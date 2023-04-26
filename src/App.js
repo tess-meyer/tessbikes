f88d9eafa947a2dd4deb4af1caf69d84775009ac
Tess <tessoliviameyer@gmail.com>
2023-04-26 15:57:54 +0200

    add footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import LandingPage from './components/Landing';
 import ProductList from './components/ProductList';
 import About from './components/About';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <LandingPage/>
       <ProductList/>
       <About/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { FaEnvelope, FaInstagram, FaYoutube } from 'react-icons/fa';
+
+const Footer = () => {
+  return (
+    <footer className="bg-indigo-400 text-white py-4">
+      <div className="container mx-auto flex justify-center items-center">
+        <div className="flex items-center">
+          <a href="mailto:info.tessbikes@gmail.com" className="mr-4">
+            <FaEnvelope className="text-white" size={30} />
+          </a>
+          <a href="https://www.instagram.com/tessbikes/" target="_blank" rel="noreferrer" className="mr-4">
+            <FaInstagram className="text-white" size={30} />
+          </a>
+          <a href="https://youtu.be/Rddgk37mAFQ" target="_blank" rel="noreferrer">
+            <FaYoutube className="text-white" size={30} />
+          </a>
+        </div>
+      </div>
+      <p className="text-sm text-white-600 mt-2 text-center">© 2023 Tess Meyer. All rights reserved. Website by Tessbikes.</p>
+    </footer>
+  );
+};
+
+export default Footer;
